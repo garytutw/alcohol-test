832f720f70ae098f790bc5ef942c6e4a2a93e6cf
Bryan <bryan0817@gmail.com>
2012-09-28 00:57:10 +0800

    Enhance login/logout flows

--- lib/app/bootstrap.rb
+++ lib/app/bootstrap.rb
@@ -11,12 +11,6 @@ end
 # Helper libraries
 Dir["#{root_dir}/helpers/*.rb"].each {|f| require_relative f}
 
-# Routes
-Dir["#{root_dir}/routes/*.rb"].each {|f| require_relative f}
-
-# Cron jobs
-Dir["#{root_dir}/crons/*.rb"].each {|f| require_relative f}
-
 class Application < Sinatra::Base
 
   ## Sinatra Settings ##
@@ -25,8 +19,30 @@ class Application < Sinatra::Base
   set :root, "#{root_dir}"
   use Rack::Session::Cookie, :secret => 'superdupersecret'
   helpers Helpers
+  register Sinatra::Flash
+	
+	# define ACL routing condition
+	set(:auth) do |*roles|   # <- notice the splat here
+	  condition do
+	   	unless logged_in? && roles.any? {|role| current_user.in_role? role }
+	   		session[:redirect_to] ||= @env["REQUEST_URI"].to_s
+	   		if !logged_in?
+	     		redirect "/login", 303
+	     	else
+	     		flash[:warning] = "You are not authorize to access this page!"
+	     	  redirect "/", 303
+	     	end
+	   	end
+	  end
+	end
 
 end
+
+# Routes
+Dir["#{root_dir}/routes/*.rb"].each {|f| require_relative f}
+
+# Cron jobs
+Dir["#{root_dir}/crons/*.rb"].each {|f| require_relative f}
 
 
 
