e7c52a2290ec529643d1e106bc9c9dc7415aa522
bryan0817 <bryan0817@gmail.com>
2012-10-19 10:02:53 +0800

    Mail handler some revises

--- lib/app/crons/mail_handler.rb
+++ lib/app/crons/mail_handler.rb
@@ -17,49 +17,34 @@ Mail.defaults do
                           :password   => 'cow3xiao',
                           :enable_ssl => false
 end
-
-Mail.find_and_delete(:what => :first, :order => :asc, :count => 50) { |mail| 
-  mail.skip_deletion
+### Use Mail.all for production ~
+Mail.find(:what => :first, :order => :asc, :count => 50, :keys => MAIL_SUBJECT_KEYWORD) { |mail|
+  #mail.skip_deletion
   begin
-    title = mail.subject
-    unless title.include? MAIL_SUBJECT_KEYWORD then
-      mail.skip_deletion
-      puts "Not a relevent mail ~ #{title}"
-      next
-    end
-    
-    body = mail.text_part.decoded
-=begin puts "##############\n"
-    body.each_line do |l|
-      r = l.split(':')
-      puts "#{r[0].strip!}, #{r[1]}"
-=end
-    
+    body = mail.text_part.decoded    
     record = Hash.new
+    tester = Hash.new
     tmp = []
-    #title.sub(MAIL_SUBJECT_KEYWORD, '').split.each {|attr|
-      #r = attr.split(':')
+    
     body.each_line do |l|
       r = l.split(':')  
-      #puts "#{r[0]}, #{r[1]}"
-      case r[0].strip!
+      r.each {|v| v.strip!} # remove empty trail
+      case r[0]
       when '員工編號'
-        driver = Driver.first_or_create(:serial => Integer(r[1]))
-        record[:driver] = driver
+        tester[:serial] = r[1]
       when '員工姓名'
-        # do nothing here 
+        tester[:name] = r[1]
       when '檢測結果'
-        record[:value] = Float(r[1].strip!)
+        record[:value] = Float(r[1])
       when '日期', '時間'
-        if r[0] == '日期' then tmp[0] = r[1].strip
-        else tmp[1] = r[1] + ':' + r[2] + ':' + r[3].strip
+        if r[0] == '日期' then tmp[0] = r[1]
+        else tmp[1] = r[1] + ':' + r[2] + ':' + r[3]
         end
         if tmp.length == 2    
           record[:time] = Time.parse(tmp[0] + ' ' + tmp[1])
         end  
       when '檢測地點'
-        site = Site.first_or_create({ :name => r[1].strip}, { :seq => 1})
-        record[:site] = site   
+        record[:site] = Site.first_or_create({ :name => r[1]}, { :seq => 1})
       else
         raise "Bad email content which contains: #{r[0]} \n"
       end
@@ -71,23 +56,30 @@ Mail.find_and_delete(:what => :first, :order => :asc, :count => 50) { |mail|
       if (attachment.content_type.start_with?('image/'))
         filename = attachment.filename
         begin
+          create_directory_if_not_exists(File.join(IMG_DIR,''))
           pathName = create_directory_if_not_exists(File.join(IMG_DIR, filename[0..1]))
           pathName = create_directory_if_not_exists(File.join(pathName, filename[2..3]))
           File.open(pathName + File::SEPARATOR + filename, "w+b", 0644) {|f| f.write attachment.body.decoded}
         rescue Exception => e
-          #mail.skip_deletion
           raise "Unable to save data for imgage: #{filename} because #{e.message}"
         end
       end
     end # end attachment
-    record[:image] = filename
+ 
+    record[:driver] = Driver.first_or_create(tester)
+    record[:image] = filename  
     puts "[Debug] Inserting alcohol test record: #{record}"
     at = AlcoholTest.new(record)
-    raise "Unable to save alcohol test record: #{at.errors.full_messages }" unless at.save
+    if at.save
+      puts "Mark this mail to be deleted!"
+#     mail.mark_for_delete
+    else
+      raise "Unable to save alcohol test record: #{at.errors.full_messages }"
+    end     
   rescue Exception => e
-    mail.skip_deletion
     # [Todo] save maleformat email to file for further processing ~
-    print "Error receiving email at " + Time.now.to_s + "::: " + e.message + "\n"   
+    print "Error receiving email at " + Time.now.to_s + "::: " + e.message + "\n"
+    next   
   end 
 } #end
-puts 'No more mail found!'
+puts 'Done! No more mail found!'
