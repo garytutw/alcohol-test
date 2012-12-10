c952eecd29c9fb886f14e064839efdf58846662b
Gary Tu <garytu@gmail.com>
2012-12-11 00:51:40 +0800

    Display photo for the anomaly tests

--- lib/app/routes/anomaly.rb
+++ lib/app/routes/anomaly.rb
@@ -3,7 +3,6 @@ class Application
   get '/anomaly/:site_id/:date', :auth => :hq do
     @date = parse_date(params[:date])
     sr = SiteReport.first(:date => @date, :site_id => params[:site_id])
-    @site_name = sr.site.name
     @tests = sr.all_anomaly_tests
     show :anomaly, :layout => false
   end
