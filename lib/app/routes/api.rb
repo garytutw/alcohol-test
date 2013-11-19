fb5116075b01ef1e2c31059ffbddae647c72646d
Gary Tu <garytu@gmail.com>
2013-11-19 23:31:14 +0800

    Check anomaly on api

--- lib/app/routes/api.rb
+++ lib/app/routes/api.rb
@@ -1,9 +1,12 @@
 # encoding: utf-8
 require 'RMagick'
+require 'mail'
 
 class Application
 
   IMG_DIR = 'photos'
+  @config = YAML.load_file('config/mail.yaml')
+  @abncfg = YAML.load_file('config/app.yaml')
 
   put '/MASTER.TDB' do
     filename = 'lib/app/public/MASTER.TDB'
@@ -40,6 +43,31 @@ class Application
     return directory_name
   end
 
+  def check_anomaly(record, limit, rule)
+    puts "[Debug] Checking anomaly rules: record[:value] > limit => #{record[:value] > limit} && !!(record[:driver].serial.to_s =~ rule) => #{!!(record[:driver].serial.to_s =~ rule)}"
+    begin
+      if record[:value] > limit && !!(record[:driver].serial.to_s =~ rule)
+        filename = record[:image]
+        # @email = Mail.new do
+        #   from    'alcoholtest@ubus.com.tw'
+        #   to      get_recipients record[:site].id
+        #   subject "[異常酒精檢測結果] 員工編號:#{record[:driver].serial} 員工姓名:#{record[:driver].name}" + 
+        #   " 檢測結果:#{record[:value]} 日期時間:#{record[:time]} 檢測地點:#{record[:site].name}"
+        #   text_part do
+        #     content_type 'text/plain; charset=UTF-8'
+        #     body    "酒測圖片如附件"
+        #   end
+        #   add_file "#{File.join(File.dirname(__FILE__), '../../..', IMG_DIR, filename[0..1], filename[2..3], filename)}"
+        # end
+        # @email.deliver!
+        puts "[Debug] Send Anomaly Mail Done!!"
+      end # end if
+    rescue Exception => e
+      # do nothing ~
+      print "Error sending anomaly email at " + Time.now.to_s + "::: " + e.message + "\n"
+    end
+  end
+
   post '/add_alc' do
     site = Site.first_or_create({:name => params["site"].strip}, {:seq => 1})
     driver = Driver.first_or_create({:serial => params["id"]}, {:name => params["name"]})
@@ -73,10 +101,21 @@ class Application
       end
     end 
     filename = filename || "NA"
-    if AlcoholTest.first_or_create(
-      {:driver => driver, :site => site, :time => DateTime.iso8601(params["timestamp"])},
-      {:value => params["alc"], :image => filename})
-      status 200
+    record = Hash.new
+    record[:driver] = driver
+    record[:site] = site
+    record[:time] = DateTime.iso8601(params["timestamp"])
+    record[:value] = BigDecimal(params["alc"])
+    record[:image] = filename
+    at = AlcoholTest.first({:driver => driver, :site => site, :time => record[:time]})
+    if not at
+      at = AlcoholTest.new(record)
+      if at.save
+        check_anomaly(record, Float(@abncfg["anomaly_bound"]), Regexp.new(@abncfg["tester_serial"]))
+        status 200
+      else
+        status 500
+      end
     else
       status 500
     end
