074f0d60cb5f8e79593bb050e64e2fb3b8f9e788
Bryan Liu <liubryan@BryanMBP.local>
2015-01-23 02:21:31 +0800

    No docker command output to web server log

--- lib/tools/csv2mdb/csv2master.sh
+++ lib/tools/csv2mdb/csv2master.sh
@@ -1,5 +1,6 @@
-docker run -v /srv/www/alcohol-test/lib/tools/csv2mdb:/src/bin wine_csv2mdb:20141229 sh /src/bin/convert.sh
+docker run -v /srv/www/alcohol-test/lib/tools/csv2mdb:/src/bin wine_csv2mdb:20141229 sh /src/bin/convert.sh > /dev/null 2>&1
 mv ./lib/tools/csv2mdb/master.mdb ./lib/app/public/MASTER.TDB
 echo 'Done!' >> ./lib/tools/csv2mdb/log.out
 docker rm $(docker ps -a -q)
 # cleanup
+rm -f ./lib/tools/csv2mdb/master_import.csv
