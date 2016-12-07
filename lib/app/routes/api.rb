9daa9f0e5084f82f14a45afb54f162bd316c9a30
Gary Tu <garytu@tw.ibm.com>
2016-12-08 00:24:54 +0800

    Add mobile input interface

--- lib/app/routes/api.rb
+++ lib/app/routes/api.rb
@@ -1,12 +1,13 @@
 # encoding: utf-8
 require 'RMagick'
 require 'mail'
+require 'securerandom'
 
 class Application
 
   IMG_DIR = 'photos'
   @@abncfg = YAML.load_file('config/app.yaml')
-  
+
   put '/MASTER.TDB' do
     filename = 'lib/app/public/MASTER.TDB'
     username = request.env['HTTP_USERNAME']
@@ -31,7 +32,7 @@ class Application
       driver.update(:name => params["name"])
     end
     if AlcoholTest.first({:driver => driver, :site => site, :time => DateTime.iso8601(params["timestamp"])})
-      json :result => "exist" 
+      json :result => "exist"
     else
       json :result => "notexist"
     end
@@ -51,11 +52,11 @@ class Application
         SiteNotifier.all(:site_id => record[:site].id).each do |notify|
           cclist += notify.email + ","
         end
-        cclist.chomp(",")  
+        cclist.chomp(",")
         @email = Mail.new do
           from    'alcoholtest@ubus.com.tw'
           to      cclist
-          subject "[異常酒精檢測結果] 員工編號:#{record[:driver].serial} 員工姓名:#{record[:driver].name}" + 
+          subject "[異常酒精檢測結果] 員工編號:#{record[:driver].serial} 員工姓名:#{record[:driver].name}" +
           " 檢測結果:#{record[:value]} 日期時間:#{record[:time]} 檢測地點:#{record[:site].name}"
           text_part do
             content_type 'text/plain; charset=UTF-8'
@@ -69,6 +70,27 @@ class Application
     rescue Exception => e
       # do nothing ~
       print "Error sending anomaly email at " + Time.now.to_s + "::: " + e.message + "\n"
+    end
+  end
+
+  def add_alc(driver, site, timestamp, value, filename)
+    record = Hash.new
+    record[:driver] = driver
+    record[:site] = site
+    record[:time] = timestamp
+    record[:value] = value
+    record[:image] = filename
+    at = AlcoholTest.first({:driver => driver, :site => site, :time => record[:time]})
+    if not at
+      at = AlcoholTest.new(record)
+      if at.save
+        check_anomaly(record, Float(@@abncfg["anomaly_bound"]), Regexp.new(@@abncfg["tester_serial"]))
+        return 200
+      else
+        return 500
+      end
+    else
+      return 500
     end
   end
 
@@ -91,7 +113,7 @@ class Application
         bg.annotate(txt, 0, 0, 0, 0, "酒測值: #{"%.3f" % params["alc"].to_f}") {
           txt.font = 'lib/app/fonts/DroidSansFallback.ttf'
           txt.gravity = Magick::CenterGravity
-          txt.pointsize = 30 
+          txt.pointsize = 30
           txt.stroke = 'transparent'
           txt.fill = '#ff0000'
           txt.font_weight = Magick::BoldWeight
@@ -103,25 +125,54 @@ class Application
         puts "Unable to save data for image: #{filename} because #{e.message}"
         return
       end
-    end 
+    end
     filename = filename || "NA"
-    record = Hash.new
-    record[:driver] = driver
-    record[:site] = site
-    record[:time] = DateTime.iso8601(params["timestamp"])
-    record[:value] = BigDecimal(params["alc"])
-    record[:image] = filename
-    at = AlcoholTest.first({:driver => driver, :site => site, :time => record[:time]})
-    if not at
-      at = AlcoholTest.new(record)
-      if at.save
-        check_anomaly(record, Float(@@abncfg["anomaly_bound"]), Regexp.new(@@abncfg["tester_serial"]))
-        status 200
-      else
+    timestamp = DateTime.iso8601(params["timestamp"])
+    value = BigDecimal(params["alc"])
+    code = add_alc(driver, site, timestamp, value, filename)
+    status code
+  end
+
+  mFields = {
+    'ID' => :id,
+    '檢測結果' => :value,
+    '日期' => :date,
+    '時間' => :time
+  }
+  post '/addm' do
+    data = {}
+    body = params['BODY']
+    body.split(/\r?\n|\r/).each do |line|
+      m = /^([^\t]+)\t?:(.+)$/.match(line)
+      next if not m
+      next if not mFields[m[1]]
+      data[mFields[m[1]]] = m[2].strip
+    end
+    site = Site.first_or_create({:name => @@abncfg["mobile_site"].strip}, {:seq => 1})
+    driver = Driver.first_or_create({:serial => data[:id]})
+    timestamp = "#{data[:date].gsub(/\//, '-')}T#{data[:time]}+08:00"
+    value = BigDecimal(data[:value])
+    filename = ''
+    if params['FILE']
+      begin
+        filename = "#{SecureRandom.urlsafe_base64}.jpg"
+        tempfile = params['FILE'][:tempfile]
+        img = tempfile.read
+        create_directory_if_not_exists(File.join(IMG_DIR,''))
+        pathName = create_directory_if_not_exists(File.join(IMG_DIR, filename[0..1]))
+        pathName = create_directory_if_not_exists(File.join(pathName, filename[2..3]))
+        File.open(pathName + File::SEPARATOR + filename, "w+b", 0644) {|f| f.write img}
+      rescue
         status 500
+        puts "Unable to save data for image: #{filename} because #{e.message}"
+        return '1'
       end
+    end
+    code = add_alc(driver, site, timestamp, value, filename)
+    if code == 200
+      '0'
     else
-      status 500
+      '1'
     end
   end
 end
